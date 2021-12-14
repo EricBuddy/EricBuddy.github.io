ad9ecaa1361a0e883cec377e37f871b105a25c2a
EricBuddy <Eric.Buddy00@gmail.com>
2021-12-14 15:17:06 -0500

    finish the home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import './Home.css'
+import picture  from '../../avatars.jpeg'
 function Home() {
     return (
-        <div>
-           name
+        <div className='home'>
+           
+
+           <img className='pic' src={picture}/>
+           
+            <div className='statements'>
+
+                Hi, my name is Eric Buddy.<br/>
+                Welcome to my react porfoilo. <br/>
+                I could do web development and code in java.
+            
+            </div>
+
         </div>
     )
 }
