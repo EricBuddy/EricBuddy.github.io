bd789ae74ea09ffa4d7bd724b06ab56e872907f5
EricBuddy <Eric.Buddy00@gmail.com>
2021-12-14 09:41:13 -0500

    testing nav bar display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './Eric.jpg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        My name is Eric Buddy and I'm going to master React!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/EricBuddy"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check Me Out!
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
