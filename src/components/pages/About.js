2c11dbcd7052c5e88257d0bc5be72ac9321a3c88
EricBuddy <Eric.Buddy00@gmail.com>
2021-12-18 17:24:40 -0500

    finish the about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import './About.css'
 function About() {
     return (
-        <div >
-            
+        <div className='about'>
+            <div className='msg'>
+                About Me<br/><br/>
+                Hi my name is Eric Buddy. 
+                I am very passionate about Computer Science. 
+                If I don't the answer to a problem or an error in a code, I will find the answer to the problem or fix the error in the code. <br/><br/>
+                Education<br/><br/>
+                Studying: Lehman College<br/>
+                Major: Computer Science <br/>
+                Degree: B.S <br/>
+                Excepted to graduate in Spring 2022
+
+            </div>
         </div>
     )
 }
